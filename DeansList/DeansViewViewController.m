73dd64cd364c1db3025e023e86b0aec5fc1f01a0
spacedrabbit <louis.tur@gmail.com>
2014-02-11 02:39:52 -0500

    Final Commit for Submission
    Added a UITextView that displays the console output
    Fixed a bug causing the ‘Student’ button to crash

--- DeansList/DeansViewViewController.m
+++ DeansList/DeansViewViewController.m
@@ -19,6 +19,8 @@
 
 @property (strong, nonatomic) University * nyu;
 
+@property (strong, nonatomic) IBOutlet UITextView *consoleOutput;
+
 @end
 
 @implementation DeansViewViewController
@@ -106,13 +108,13 @@
             if ( [depts isEqual:compSciDept] ){
                 [crs setTeacher:pPark];
                 [crs addAssistantTeachers:@[lTur, eWang]];
-                //[self.nyu addCourseID:crs withName:crs.courseName];
+                [self.nyu addCourseID:crs withName:crs.courseName];
             }else if ([depts isEqual:bioDept]){
                 [crs setTeacher:lTur];
-                //[self.nyu addCourseID:crs withName:crs.courseName];
+                [self.nyu addCourseID:crs withName:crs.courseName];
             }else if ( [depts isEqual:chemDept] ){
                 [crs setTeacher:eWang];
-                //[self.nyu addCourseID:crs withName:crs.courseName];
+                [self.nyu addCourseID:crs withName:crs.courseName];
             }else{
                 NSLog(@"Unknown Department Found. Cannot Add %@", crs);
             }
@@ -128,6 +130,7 @@
     NSLog(@"%@ enrolled the following students: %@", self.nyu, self.nyu.enrolledStudents);
     NSLog(@"%@ has the following departments: %@", self.nyu, self.nyu.departments);
     
+    
     NSLog(@"The %@ has the following Courses: %@", compSciDept, compSciDept.courses);
     NSLog(@"The %@ has the following Courses: %@", bioDept, bioDept.courses);
     NSLog(@"The %@ has the following Courses: %@", chemDept, chemDept.courses);
@@ -140,15 +143,43 @@
     NSLog(@"%@ is taught by %@", bio101, bio101.teacher);
     NSLog(@"%@ is taught by %@", chem101, chem101.teacher);
     
+    
     NSLog(@"%@ is enrolled in: %@", jNorris, jNorris.associatedCourses);
     NSLog(@"%@ is enrolled in: %@", dWallace, dWallace.associatedCourses);
     NSLog(@"%@ is enrolled in: %@", sKoonin, sKoonin.associatedCourses);
-     
-
+    
     NSLog(@"%@ teaches the following courses: %@", pPark, pPark.associatedCourses);
     NSLog(@"%@ teaches the following courses: %@", lTur, lTur.associatedCourses);
     
     NSLog(@"The %@ course has the following assistant teachers:%@", compSci201, compSci201.assistantTeachers);
+    
+    NSMutableString *consoleText = [NSMutableString new];
+    [consoleText appendFormat:@"> Console Output\n\n> %@ employs: %@\n\n> %@ enrolled the following students: %@\n\n> %@ has the following departments: %@\n\n> The %@ has the following Courses: %@\n\n> The %@ has the following Courses: %@\n\n> The %@ has the following Courses: %@\n\n> %@ enrolled students: %@\n\n> %@ enrolled students: %@\n\n> %@ enrolled students: %@\n\n> %@ is taught by %@\n\n> %@ is taught by %@\n\n> %@ is taught by %@\n\n> %@ is enrolled in: %@\n\n> %@ is enrolled in: %@\n\n> %@ is enrolled in: %@\n\n> %@ teaches the following courses: %@\n\n> %@ teaches the following courses: %@\n\n> The %@ course has the following assistant teachers:%@",
+     self.nyu, self.nyu.hiredStaff,
+     self.nyu, self.nyu.enrolledStudents,
+     self.nyu, self.nyu.departments,
+     compSciDept, compSciDept.courses,
+     bioDept, bioDept.courses,
+     chemDept,chemDept.courses,
+     compSci101, compSci101.students,
+     compSci201, compSci201.students,
+     compSci202, compSci202.students,
+     compSci101, compSci101.teacher,
+     bio101, bio101.teacher,
+     chem101, chem101.teacher,
+     jNorris, jNorris.associatedCourses,
+     dWallace, dWallace.associatedCourses,
+     sKoonin, sKoonin.associatedCourses,
+     pPark, pPark.associatedCourses,
+     lTur, lTur.associatedCourses,
+     compSci201, compSci201.assistantTeachers];
+    
+    self.consoleOutput.text = consoleText;
+    
+    self.consoleOutput.textColor = [UIColor greenColor];
+    self.consoleOutput.font = [UIFont fontWithName:@"Menlo" size:13.0];
+    self.consoleOutput.userInteractionEnabled = TRUE;
+    self.consoleOutput.showsVerticalScrollIndicator = TRUE;
 
 }
 
